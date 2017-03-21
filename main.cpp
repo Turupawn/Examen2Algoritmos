e5815fe175d3761c436071f83c244e751e806f47
Turupawn <ahmed.hn.43@gmail.com>
2017-03-21 16:35:02 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "Test.h"
+#include <iostream>
+
+using namespace std;
+
+//Counts the number of posibles paths from origin to destination in the given DAG (Directed Acyclic Graph)
+//Use Dynamic Programing to optimize the process
+int countPaths(int** DAG, int size, int origin, int destination)
+{
+    return -1;
+}
+
+//Returns a vector that maps each vertex to a color (or number)
+//Two adjacent vertex cannot have the same color
+//The less colors used, the better
+//Use a Greedy Paradigm to optimize the process
+//The best solution is not required
+vector<int> getVertexColors(int** graph, int size)
+{
+    vector<int> answer;
+    return answer;
+}
+
+int main ()
+{
+    test();
+    return 0;
+}
